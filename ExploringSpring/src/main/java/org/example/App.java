d29c92a3b789a43781ffab57531fd7ce7e29ec73
abdelrahman elsayed attia <166997051+Abdoh1100@users.noreply.github.com>
2025-02-21 09:59:25 +0100

    innner Bean

--- ExploringSpring/src/main/java/org/example/App.java
+++ ExploringSpring/src/main/java/org/example/App.java
@@ -13,9 +13,13 @@ public class App
     {
         ApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
-        programmer pro=(programmer) context.getBean("programmer");
+//        programmer pro=(programmer) context.getBean("programmer");
+//        programmer pro =context.getBean("programmer", programmer.class);// no need for type casting now !
+
+        programmer pro =(programmer)context.getBean( "programmer");// no need for neither type casting nor name now !
         System.out.println(pro.getAge());
         pro.getComp().compile();
-        
+
+//        computer com=context.getBean(computer.class);
     }
 }
--- ExploringSpring/src/main/java/org/example/Desktop.java
+++ ExploringSpring/src/main/java/org/example/Desktop.java
@@ -1,6 +1,10 @@
 package org.example;
 
 public class Desktop implements computer {
+
+    public Desktop(){
+        System.out.println("Desktop object is created !");
+    }
     @Override
     public void compile() {
         System.out.println("Compiling... Desktop");

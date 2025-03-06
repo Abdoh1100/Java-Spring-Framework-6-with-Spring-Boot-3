56e7311ac365e013c9c481bdf33b6c61348a26af
abdelrahman elsayed attia <166997051+Abdoh1100@users.noreply.github.com>
2025-03-06 15:11:18 +0100

    Spring boot
    initializing spring boot app

---
+++ Section 9/Section10/src/main/java/org/example/App.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.springframework.context.ApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+
+        System.out.println( "Hello World!" );
+    }
+}

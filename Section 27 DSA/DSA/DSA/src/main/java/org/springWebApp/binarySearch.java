a6dfdaccde97cd192df15b470febce91d12f50f7
abdelrahman elsayed attia <166997051+Abdoh1100@users.noreply.github.com>
2025-03-24 13:53:19 +0100

    Update binarySearch.java

--- Section 27 DSA/DSA/DSA/src/main/java/org/springWebApp/binarySearch.java
+++ Section 27 DSA/DSA/DSA/src/main/java/org/springWebApp/binarySearch.java
@@ -3,6 +3,23 @@ package org.springWebApp;
 public class binarySearch {
 
 
+    public static int BSIterative(int []nums,int target,int left,int right){
+
+        while(left<=right){
+            int mid =left+(right-left)/2;
+            if(nums[mid]==target)
+                return mid;
+            if(nums[mid]<target){
+                left=mid+1;
+            }
+            if(nums[mid]>target){
+                right=mid-1;
+
+            }
+
+        }
+        return -1;
+    }
     public static int BS(int [] nums,int target,int left,int right){
 
 
@@ -31,7 +48,8 @@ public class binarySearch {
     public static void main (String args[]){
 
         int nums[]={1,3,4,5,7,9,12};
-        int index=BS(nums,7,0,nums.length-1);
+//        int index=BS(nums,7,0,nums.length-1);
+        int index=BSIterative(nums,12,0,nums.length-1);
         System.out.println(index);
 
     }
